1a830dddc70e9661698d82390329d1059cb6d45c
yolana <yolana229@gmail.com>
2025-05-27 14:47:50 -0300

    estilos nuevos

--- ecomerce/index.js
+++ ecomerce/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Para importar Bootstrap
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import './index.css'; 
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
